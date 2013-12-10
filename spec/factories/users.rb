f6b2c9fbf4e50ad1417b0212ce9ede5d468ec6f4
Sameer Siruguri <siruguri@gmail.com>
2013-12-10 15:31:42 -0800

    put in a successful RSpec test!

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    email 'sameer@sameer.me'
-    password 'sameer123'
-    password_confirmation 'sameer123'
-    # required if the Devise Confirmable module is used
-    # confirmed_at Time.now
-  end
-end
